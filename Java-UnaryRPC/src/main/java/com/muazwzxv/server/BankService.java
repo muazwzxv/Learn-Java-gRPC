9489e6e68bb2cc2c79fd9ab091fd65858d4b9905
muazwzxv <muazwazir@gmail.com>
2021-12-22 00:20:33 +0800

    proto service definition and withdraw method implementation

--- Java-UnaryRPC/src/main/java/com/muazwzxv/server/BankService.java
+++ Java-UnaryRPC/src/main/java/com/muazwzxv/server/BankService.java
@@ -1,9 +1,7 @@
 package com.muazwzxv.server;
 
 import com.google.protobuf.Int32Value;
-import com.muazwzxv.models.Balance;
-import com.muazwzxv.models.BalanceCheckRequest;
-import com.muazwzxv.models.BankServiceGrpc;
+import com.muazwzxv.models.*;
 import io.grpc.stub.StreamObserver;
 
 public class BankService extends BankServiceGrpc.BankServiceImplBase {
@@ -19,6 +17,23 @@ public class BankService extends BankServiceGrpc.BankServiceImplBase {
                         .build())
                 .build();
         responseObserver.onNext(balance);
+        responseObserver.onCompleted();
+    }
+
+    @Override
+    public void withdraw(WithdrawRequest request, StreamObserver<Money> responseObserver) {
+        int accountNumber = request.getAccountNumber();
+        // We assume all amount request are multiple of 10
+        // 10 - 20 - 30 - 40
+        int amount = request.getAmount();
+        int balance = AccountDatabase.getBalance(accountNumber);
+
+        // Server will stream 10 dollar for each iteration of the loop
+        for (int i = 0; i < (amount / 10); i++) {
+            Money money = Money.newBuilder().setValue(10).build();
+            responseObserver.onNext(money);
+        }
+        // Server completed the request
         responseObserver.onCompleted();
     }
 }
