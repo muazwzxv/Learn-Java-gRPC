3466b19c2483b9cf9ba64a862b47efb63200c1c9
muazwzxv <muazwazir@gmail.com>
2021-12-24 01:10:44 +0800

    transfer service client test

---
+++ Java-gRPC-BankService/src/test/java/com/muazwzxv/client/TransferClientTest.java
@@ -0,0 +1,49 @@
+package com.muazwzxv.client;
+
+
+import com.muazwzxv.client.observer.TestTransferStreamResponse;
+import com.muazwzxv.models.TransferRequest;
+import com.muazwzxv.models.TransferServiceGrpc;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import io.grpc.stub.StreamObserver;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class TransferClientTest {
+
+    private TransferServiceGrpc.TransferServiceStub stub;
+
+    @BeforeAll
+    public void setup() {
+
+        ManagedChannel managedChannel = ManagedChannelBuilder.forAddress("localhost", 8000)
+                .usePlaintext()
+                .build();
+
+        this.stub = TransferServiceGrpc.newStub(managedChannel);
+    }
+
+    @Test
+    public void transfer() throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
+        TestTransferStreamResponse res = new TestTransferStreamResponse(latch);
+        StreamObserver<TransferRequest> requestStreamObserver = this.stub.transfer(res);
+
+        for (int i = 0; i < 100; i++) {
+            TransferRequest transferRequest = TransferRequest.newBuilder()
+                    .setFromAccount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .setToAccount(ThreadLocalRandom.current().nextInt(1, 11))
+                    .setAmount(40)
+                    .build();
+            requestStreamObserver.onNext(transferRequest);
+        }
+        requestStreamObserver.onCompleted();
+        latch.await();
+    }
+}
