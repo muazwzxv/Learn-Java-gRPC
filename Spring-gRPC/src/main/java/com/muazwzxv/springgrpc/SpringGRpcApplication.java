443b3b79ce4ad6c36a31e128e73cde6b62fce94b
muazwzxv <muazwazir@gmail.com>
2022-01-25 13:38:02 +0800

    setup spring boot

---
+++ Spring-gRPC/src/main/java/com/muazwzxv/springgrpc/SpringGRpcApplication.java
@@ -0,0 +1,13 @@
+package com.muazwzxv.springgrpc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGRpcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringGRpcApplication.class, args);
+    }
+
+}
